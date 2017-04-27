fce035865b1e5b4613560ebe26d07f1cf1badcea
xuexk <nolanctt@hotmail.com>
2017-04-27 10:32:14 +0800

    update template core

---
+++ util/core/installTemplate.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const exec = require('../exec.js')
+const fs = require('fs')
+const hslog = require('../hslog.js')
+module.exports = function(template) {
+    const templatePath = require(template).templatePath
+    if (fs.existsSync(path.join(templatePath, 'config.json'))) {
+        if (process.platform == 'win32') {
+            exec('XCOPY ' + path.join(templatePath, 'templates/*') + process.cwd() + " /E")
+        } else {
+            exec('cp -R ' + path.join(templatePath, 'templates') + '/* ' + process.cwd())
+        }
+    } else {
+        hslog.error(`无效的模版目录:${templatePath}`)
+    }
+
+}
