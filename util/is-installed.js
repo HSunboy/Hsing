3dcfdaee864297461da24a0ad2eb380d59c3a0d8
xuexk <nolanctt@hotmail.com>
2017-04-28 09:20:14 +0800

    bug fixed

--- util/is-installed.js
+++ util/is-installed.js
@@ -1,11 +1,11 @@
 'use strict'
-
+const cp = require('child_process')
 module.exports = name => {
-  try {
-    require.resolve(name)
+    try {
+        cp.execSync("node -e require.resolve('" + name + "')", { stdio: 'ignore' });
 
-    return true
-  } catch (_) {
-    return false
-  }
+        return true
+    } catch (_) {
+        return false
+    }
 }
